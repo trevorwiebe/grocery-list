3a95b779d9f22626654f12d343f08d927883d44f
Trevor W <tw@trevorwiebe.com>
2025-10-12 14:51:38 -0500

    add database seeds

---
+++ seeds.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const { 
+    GroceryList,
+    Item,
+    Category,
+    SubCategory
+ } = require('./models/modelSchemas')
+
+mongoose.connect('mongodb://localhost:27017/grocery-list')
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const itemList = [
+    "Eggs", "Milk", "Cheese", "Flour", "Sugar", "Coffee", "Butter", "Apples", "Carrots"
+]
+
+const seedDB = async () => {
+
+    // Clear the exisisting data
+    await GroceryList.deleteMany({});
+    await Item.deleteMany({});
+    await Category.deleteMany({});
+    await SubCategory.deleteMany({});
+
+    // Add items
+    for(let i in itemList){
+        const item = new Item({
+            name: itemList[i],
+            subCategoryId: "abc"
+        })
+
+        await item.save();
+    }
+
+    // Add new category
+    const subCategory = new SubCategory({
+        name: "Meat and cheese drawer"
+    })
+    const category = new Category({
+        name: "Fridge"
+    })
+    category.subCategories.push(subCategory);
+    await category.save();
+    await subCategory.save();
+
+    db.close()
+    
+}
+
+seedDB();

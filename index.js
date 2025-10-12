298f836f42f7448d1514f0ed677522d29338b006
Trevor W <tw@trevorwiebe.com>
2025-10-12 16:49:39 -0500

    move item views in to item folder, add ability to update item

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose');
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
 
 const { GroceryList, Item } = require('./models/modelSchemas');
 
@@ -19,6 +20,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.engine('ejs', ejsMate);
 
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
     res.render('home');
 })
@@ -30,8 +34,20 @@ app.get('/lists', async(req, res) => {
 
 app.get('/items', async (req, res) => {
     const items = await Item.find({})
-    console.log(items);
-    res.render('showItems', {items});
+    res.render('items/show', {items});
+})
+
+app.get('/items/:id/edit', async (req, res) => {
+    const item = await Item.findById(req.params.id);
+    res.render('items/edit', {item})
+})
+
+app.put('/items/:id', async (req, res) => {
+    await Item.findByIdAndUpdate(
+        req.params.id, 
+        { name: req.body.item.name }
+    );
+    res.redirect(`/items`);
 })
 
 app.get('/categories', async (req, res) => {
